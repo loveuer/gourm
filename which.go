bf741ef518abf2c078092793bac75b977c2e3abb
loveuer <loveuer@live.com>
2020-01-08 14:22:14 +0800

    add which can define column custom

--- which.go
+++ which.go
@@ -24,13 +24,19 @@ func (d *DB) Which(out interface{}, outPrimaryVal ...interface{}) error {
 	} else if len(outPrimaryVal) == 1 {
 		condition = fmt.Sprintf("%s = '%v'", primaryKey, outPrimaryVal[0])
 
+	} else if len(outPrimaryVal) == 2 {
+		if reflect.TypeOf(outPrimaryVal[0]).Kind() != reflect.String {
+			return fmt.Errorf("gourm which: col val must be string but => %v", reflect.TypeOf(outPrimaryVal[0]))
+		}
+		condition = fmt.Sprintf("%s = '%v'", outPrimaryVal[0].(string), outPrimaryVal[1])
+
 	} else {
 		return fmt.Errorf("primary key length not 0 or 1 but %d", len(outPrimaryVal))
 	}
 
 	var sliceout []interface{}
 
-	sentence := fmt.Sprintf("select %s from %s where %s", strings.Join(columns, ", "), getTableName(rflctVal), condition)
+	sentence := fmt.Sprintf("select %s from %s where %s limit 1", strings.Join(columns, ", "), getTableName(rflctVal), condition)
 	for i := 1; i < rflctVal.NumField(); i++ {
 		sliceout = append(sliceout, rflctVal.Field(i).Addr().Interface())
 	}
